19af051038412c262627bd2028ccf6b486359bd7
maciejGolebio <czerwoneszelki19@gmail.com>
2022-02-05 13:13:27 +0100

    Add menu positions routes

--- application/src/routes/menu_position_router.py
+++ application/src/routes/menu_position_router.py
@@ -2,17 +2,41 @@ from typing import List
 
 from bootsrap.container import get_mediator
 from dependency_injector.wiring import inject
-from domain.menu.commands.menu_position_commands import \
-    CreateMenuPositionCommand
+from domain.menu.commands.menu_position_commands import (
+    CreateMenuPositionCommand, DeleteMenuPositionCommand,
+    UpdateMenuPositionCommand)
 from domain.menu.entity.menu import MenuId
-from domain.menu.entity.menu_position import InputMenuPosition
+from domain.menu.entity.menu_position import InputMenuPosition, MenuPositionId
 from fastapi import APIRouter, Depends
 from mediator.request import LocalRequestBus
 
 router = APIRouter(tags=["menuPositions"])
 
 
-@router.post("/menu/{menu_id}/position", response_model=None)
+@router.post("/menu/{menu_id}/position", status_code=200, response_model=MenuPositionId)
 @inject
 async def create(menu_id: MenuId, menu_position: InputMenuPosition, mediator: LocalRequestBus = Depends(get_mediator)) -> MenuId:
     return await mediator.execute(CreateMenuPositionCommand(menu_id=menu_id, position=menu_position))
+
+
+@router.put("/menu/{menu_id}/position/{menu_position_id}", status_code=200, response_model=MenuPositionId)
+@inject
+async def update(menu_id: MenuId, menu_position_id: MenuPositionId, menu_position: InputMenuPosition, mediator: LocalRequestBus = Depends(get_mediator)) -> MenuId:
+    return await mediator.execute(
+        UpdateMenuPositionCommand(
+            menu_id=menu_id,
+            position=menu_position,
+            menu_position_id=menu_position_id
+        )
+    )
+
+
+@router.delete("/menu/{menu_id}/position/{menu_position_id}", status_code=200, response_model=MenuPositionId)
+@inject
+async def delete(menu_id: MenuId, menu_position_id: MenuPositionId, mediator: LocalRequestBus = Depends(get_mediator)) -> MenuId:
+    return await mediator.execute(
+        DeleteMenuPositionCommand(
+            menu_id=menu_id,
+            menu_position_id=menu_position_id
+        )
+    )
