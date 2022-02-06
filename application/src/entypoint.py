13c56a05eb61a0688a87a2073f861d7d6c6235e8
maciejGolebio <czerwoneszelki19@gmail.com>
2022-02-06 16:36:51 +0100

    Cleand up commented code

--- application/src/entypoint.py
+++ application/src/entypoint.py
@@ -3,8 +3,6 @@ from typing import List
 
 from bootsrap.container import Container, get_mediator
 from bootsrap.register_handlers import discover_handlers
-from domain.menu.command_handlers import handlers_menu_position
-# from domain.menu.query_handler import query_handlers
 
 from routes import menu_router
 from routes import menu_position_router
@@ -15,14 +13,8 @@ def create_app() -> FastAPI:
 
     container = Container()
 
-    container.wire(modules=[handlers_menu_position])
-
     bus = get_mediator()
     discover_handlers(bus, container)
-
-    bus.register(handlers_menu_position.create_menu_position_command_handler)
-    bus.register(handlers_menu_position.update_menu_position_command_handler)
-    bus.register(handlers_menu_position.delete_menu_position_command_handler)
 
     app = FastAPI()
     app.include_router(menu_router.router)
